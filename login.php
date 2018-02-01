8506eb077bcffeddd88fc2bcad85fba1528377d9
Esmeralda <34519178+Esmoves@users.noreply.github.com>
2018-02-01 17:22:01 +0100

    working login file
    Works with
    connection.php 
    user.php
    
    Uses classes to connect to database.

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php
+    include_once ('include_phpfunctions.php');
+    
+//*************************************************************************//
+//*************************************************************************//
+// ***************  Run the page!!!!!  ************************************//
+//*************************************************************************//
+//*************************************************************************//
+     // include html header
+     // contains title, mainmenu, leftmenu
+      
+      include_once ('include_html.php');
+      include_once ('user.php');
+      
+     if ( isset( $_POST['submit'] ) ){ // if form is not send show form
+     	$username = $_POST['username'];
+     	$password = $_POST['password'];
+
+     	$object = new User();
+     	$object->Login($username, $password);
+
+     	//header("location:welcome.php");
+    	}
+
+     ?>
+
+     	<form name="login" id="login" method="POST" action="login.php">
+     		<label for"username">Username</label><br />
+     		<input type="text" name="username"><br /><br />
+     		<label for="password">Password</label><br />
+     		<input type="password" name="password"><br /><br />
+     		<input type="submit" name="submit" value="submit">
+     	</form>
+
+   <!--- FOOTER  -->   
+          </div> <!-- end div main content -->
+        </div> <!-- end div all content -->
+
+      <!-- validated CSS SIGN -->
+      <div class="footer">
+        <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valide CSS!" />
+        </a>
+        </p>
+      </div>
+
+    </div> <!-- end container div -->
+  </body>
+</html>
