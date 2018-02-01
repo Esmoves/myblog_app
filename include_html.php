b6b2ec1a6aded86de82f76e015db9e72bece512f
Esmeralda <34519178+Esmoves@users.noreply.github.com>
2018-02-01 15:25:39 +0100

    include the html for the pages
    contains the header, title, meta and links.
    contains the side menu and main menu
    should be included in every page.
    finished

---
+++ include_html.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN'>
+
+    <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='nl' lang='nl'>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+        <meta name="description" content="Blog application by Esmeralda Tijhoff" />  
+        <meta name="keywords" content="" />
+        <meta name="author" content="A.E.Tijhoff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
+        <link href="./style.css"  media="all" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Pontano+Sans|Roboto+Mono" rel="stylesheet" />
+       <title>Chatt App</title>
+    </head>
+    <body>
+
+
+       <div id="container" class="container">
+          <div class="titel" id="top-header">
+            <h1>BLOG App</h1>
+          </div>
+
+        <!-- Main Menu -->
+        <div class="mainmenu">
+          <ul class="mainmenu">
+            <li><a href="index.php">home</a></li>
+            <li><a href="login.php">register</a></li>
+            <li><a href="login.php">login</a></li>
+            <li><a href="newblog.php">upload blog</a></li>
+          </ul>
+        </div>
+        <br />
+        <br />
+
+        <div class="Allcontent">
+            <!-- Left side menu div -->
+            <div class="leftmenu">
+              <h3>Authors</h3>
+              <ul class="lmenu">
+                <?php
+                  showbloggers();
+                ?>
+              </ul>
+             <h3>Categories</h3>
+             <ul class="lmenu">
+              <?php
+                showcategories();
+                ?>
+             </ul> 
+            </div>
+            <div class="maincontent">
