e622d104db61540c00ad7b89de01651a156daa75
Esmeralda <34519178+Esmoves@users.noreply.github.com>
2018-02-05 17:14:35 +0100

    added comments and form for uploaden comments

--- blog.php
+++ blog.php
@@ -2,6 +2,9 @@
 session_start();  
     include 'include_phpfunctions.php';
 
+if(!empty($_POST['submit'])){
+  insertcomment();
+}
 
 //*************************************************************************//
 //*************************************************************************//
@@ -16,6 +19,19 @@ try {
     // get blog by user_id
     $blog_id = $_GET['blog'];
     getOneBlogFromDB($blog_id);
+   
+
+    /* upload a comment form */
+    ?>
+    <form action="blog.php" method="post" name="comment" class="inputform">
+    <label for="comment">Send us your comment</label><br>
+    <input type="hidden" name="blog_id" value="<?php echo $blog_id; ?>" />
+    <textarea class="excerp" type="text" name="comment"></textarea><br />
+    <input type="submit" name="submit" value="send" /> 
+
+    <?php
+    
+    getcomments($blog_id);
   }
 
 catch(PDOException $e)
