f5a1e31e606b3f220053888c2c38381fb2c40f36
Esmeralda <34519178+Esmoves@users.noreply.github.com>
2018-02-09 11:39:50 +0100

    wk4-001 tot wk4-004
    op deze pagina kan de blogger comments deleten en een blog dicht zetten voor commenting

--- blog.php
+++ blog.php
@@ -12,49 +12,52 @@ if(!empty($_POST['submit'])){
 //*************************************************************************//
 //*************************************************************************//
 try {
-http://localhost/websites/Myblogg_app/blog.php?action=delete&blog=48&comment=7
-  // include html header
-      // contains title, mainmenu, leftmenu
+    // include html header
+    // contains title, mainmenu, leftmenu
     include 'include_html.php';
-    // get blog by user_id
     $blog_id = $_GET['blog'];
-    getOneBlogFromDB($blog_id);
 
     if(isset($_SESSION["login_user"])) {
 
       // check if a comment is deleted
-      if(!empty($_GET['action']) && !empty($_GET['comment'])){
+      if(!empty($_GET['action']) ){
 
-        if($_GET['action'] == 'delete'){ 
-
+        if($_GET['action'] == 'delete' && !empty($_GET['comment'])){ 
           $comment_id= $_GET['comment'];
           deletecomment($comment_id);
         }
-      } 
-
+        if($_GET['action'] == 'close' && !empty($_GET['blog'])){ 
+          $blog_id= $_GET['blog'];
+          closeblog($blog_id);
+        } 
+      }
       // get user id 
       $username = $_SESSION['login_user'];
       $sql_user = "SELECT * FROM bloggers WHERE username = '$username'";
       foreach($db->query($sql_user) as $user){
         $user_id = $user['id'];
-      }  
+        }  
+
        // show edit blog button
-      echo '<br /><br /><button><a href="editblog.php?action=edit&user=' .$user_id. '&blog=' .$blog_id. '">Edit this blog</a></button>';
+      echo '<br /><br /><button style="width:500px;"><a href="editblog.php?action=edit&user=' .$user_id. '&blog=' .$blog_id. '">Edit this blog</a></button>';
+
+      echo '<br /><br /><button style="width:500px;"><a href="blog.php?action=close&blog=' .$blog_id. '">Close this blog for commenting</a></button>';
 
       // show comments for blogger with delete option
       getcommentsfordelete($blog_id);
 
+       // get blog by user_id
+      getOneBlogFromDB($blog_id);
     }
 
     if(empty($_SESSION["login_user"])) {
-      /* upload a comment form */
-      ?>
-      <form action="blog.php" method="post" name="comment" class="inputform">
-      <label for="comment">Send us your comment</label><br>
-      <input type="hidden" name="blog_id" value="<?php echo $blog_id; ?>" />
-      <textarea class="excerp" type="text" name="comment"></textarea><br />
-      <input type="submit" name="submit" value="send" /> 
-      <?php     
+
+      // get blog by user_id
+      getOneBlogFromDB($blog_id);
+
+
+    
+
       // show comments for reader
       getcomments($blog_id);
     }
@@ -65,17 +68,7 @@ catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
-
-
-
-
-
 ?>
-
-
-
-
-
 
 </div>
 </body>
